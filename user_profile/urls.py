c68b7ec3e56dc4e5d9d22c153dee62c81a17c38c
Kanezal <Kanezal@yandex.ru>
2022-02-24 19:51:29 +0300

    apps added

---
+++ user_profile/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path, re_path
+
+from .views import *
+
+urlpatterns = [
+    # path('', имя_функции, name='имя_для_вызова'),
+]
+
+# Имя для вызова это сокращенное название функции, то есть должен отражаться
+# основной смысл view-функции.

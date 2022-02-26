99fbf15bad93a6109df69acae3791e1b77fab714
Stepan Detina <Stepan Detina>
2022-02-26 20:17:59 +0300

    Models done

--- messenger/models.py
+++ messenger/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.conf import settings
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Message(models.Model):
+    user_sender = models.ForeignKey(to = settings.AUTH_USER_MODEL, on_delete = models.SET_NULL, null=True, related_name = 'sender')
+    user_receiver = models.ForeignKey(to = User, on_delete = models.SET_NULL, null=True, related_name = 'receiver')
+    text = models.TextField()
+    date_receipt = models.DateTimeField()
+    

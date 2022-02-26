bcbf5676b6f585ddc57d2275a3e5a8ad80d8004c
Maria <maria@stetsenko.su>
2022-02-26 16:52:45 +0300

    apps added in settings.py

--- social_network/settings.py
+++ social_network/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'news',
+    'community',
+    'messenger',
+    'search',
+    'user_profile',
 ]
 
 MIDDLEWARE = [

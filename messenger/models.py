21039f73f1a5a38cca5144fccb33c18f832f4ca6
Stepan Detina <Stepan Detina>
2022-03-01 00:26:06 +0300

    Chat and Chat_user added, Messages changed

--- messenger/models.py
+++ messenger/models.py
@@ -7,4 +7,17 @@ class Message(models.Model):
     user_receiver = models.ForeignKey(to = User, on_delete = models.SET_NULL, null=True, related_name = 'receiver')
     text = models.TextField()
     date_receipt = models.DateTimeField()
-    
+    chat_id = models.ManyToManyField(to = Chat) # id Диалога к которому относится сообщение
+    isRead = models # прочитано ли сообщение получателем
+    isVisible_only_for_author = models# видимость сообщение для первого юзера
+    isVisible_all_users = models# видимость сообщение для первого юзера (для возможности отчисти сообщений, если просто удалять тогда сообщение пропадет из истории у обоих юзеров)
+
+class Chat:
+    title = models.CharField(max_length= 25)# заголовок чата, его название
+    creator = models.ForeignKey(to = User, on_delete=models.CASCADE)# id пользователя создавшего чат
+    icon = models.ImageField(null=True, blank=True, upload_to="images/") # иконка чата
+    date_of_creation = models.DateTimeField() # время создания
+
+class Chat_user(models.Model):
+    chat_id = models.ForeignKey(to = Chat, on_delete = models.SET_NULL)
+    user_id = models.ForeignKey(to = User, on_delete = models.SET_NULL)

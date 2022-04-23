66f2bd2016df63356853acc38bb493172deff2c5
Stepan Detina <Stepan Detina>
2022-04-23 19:21:05 +0300

    удаление сообщение сделано

--- messenger/urls.py
+++ messenger/urls.py
@@ -4,8 +4,9 @@ from .views import *
 
 urlpatterns = [
     # path('', имя_функции, name='имя_для_вызова'),
-    path('chats/chat<int:id>', messages),
+    path('chats/chat/<int:id>', messages),
     path('chats', chats),
+    path('chats/delete<int:id>', delete)
 ]
 
 # Имя для вызова это сокращенное название функции, то есть должен отражаться
--- messenger/views.py
+++ messenger/views.py
@@ -1,7 +1,8 @@
 from .models import ChatUser, Message, Chat
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import Http404, HttpResponse
 from django.views.generic import CreateView
+from django.core.exceptions import ObjectDoesNotExist
 from .forms import *
 from django.urls import reverse_lazy
 import datetime
@@ -25,24 +26,27 @@ def messages(request, id):
             has_errors = True
     else:
         form = MessageForm()
-       
-    
-    try:
-        all_messages = Message.objects.filter(chat_id=id).order_by('-date_receipt')
-        chats_user = ChatUser.objects.filter(chat_id=Chat.objects.get(id=id))
-        for chat_user in chats_user:
-            if request.user == chat_user.user_id:
-                ctx = {
-                    'messages' : all_messages,
-                    'form' : form,
-                    'has_errors': has_errors
-                }
+           
+    all_messages = Message.objects.filter(
+        chat_id=id,
+        isVisible_all_users = True
+    ).order_by('-date_receipt')
 
-                return render(request, 'messages.html', context = ctx)
-    except Exception:
-        pass
-    
-    raise Http404
+    try:
+        chats_user = ChatUser.objects.filter(chat_id=Chat.objects.get(id=id))
+    except ObjectDoesNotExist:
+        raise Http404
+
+    for chat_user in chats_user:
+        if request.user == chat_user.user_id:
+            ctx = {
+                'messages' : all_messages,
+                'form' : form,
+                'has_errors': has_errors,
+                'title' : Chat.objects.filter(id=id)
+            }
+            return render(request, 'messages.html', context = ctx)
+
 
 def chats(request):
     has_errors = False
@@ -68,8 +72,14 @@ def chats(request):
     ctx = {
         'chats' : all_chats,
         'form' : form,
-        'has_errors': has_errors
+        'has_errors': has_errors,
     }
 
     return render(request, 'chats.html', context = ctx)
-         
+
+def delete(request, id):
+    message = Message.objects.get(id=id)
+    message.isVisible_all_users = False
+    message.save()
+    return redirect(f"chat/{message.chat_id.id}")
+#request.GET.get('id')

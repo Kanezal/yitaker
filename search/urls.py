de369f1dae5e86be8064e67c8543a5b997b710d7
Maria Stetsenko <maria@stetsenko.su>
2022-05-14 17:52:47 +0300

    fixed

--- search/urls.py
+++ search/urls.py
@@ -3,8 +3,8 @@ from django.urls import path, re_path
 from .views import *
 
 urlpatterns = [
-    path('user', SearchUserView, ''),
-    path('novelty', SearchNoveltyView, ''),
-    path('community', SearchCommunityView, ''),
-    path('advanced', FilterView, ''),
+    path('user', SearchUserView, name='search_user'),
+    path('novelty', SearchNoveltyView, name='search_novelty'),
+    path('community', SearchCommunityView, name='search_community'),
+    path('advanced', FilterView, name='search_advanced'),
 ]
--- search/views.py
+++ search/views.py
@@ -6,28 +6,52 @@ from community.models import Community
 from .filters import ProfileFilter
 from .serializers import *
 
+
+def base_ctx() -> dict:
+    return {
+        "nav": {
+            "Поиск пользователя по нику": {
+                "link": "search_user"
+            },
+            "Поиск пользователя по интересам": {
+                "link": "search_advanced"
+            },
+            "Поиск новостей": {
+                "link": "search_novelty"
+            },
+            "Поиск сообществ": {
+                "link": "search_community"
+            },
+        }
+    }
+
+
 def SearchUserView(request):
+    context = base_ctx()
     users = get_user_model().objects.all()
     serializer = UserSerializer(users, many=True)
-    context = {'data': serializer.data}
+    context['data'] = serializer.data
     return render(request, 'search_user.html', context)
 
 def SearchNoveltyView(request):
+    context = base_ctx()
     novelties = Novelty.objects.all()
     serializer = NoveltySerializer(novelties, many=True)
-    context = {'data': serializer.data}
+    context['data'] = serializer.data
     return render(request, 'search_novelty.html', context)
 
 def SearchCommunityView(request):
+    context = base_ctx()
     communities = Community.objects.all()
     serializer = CommunitySerializer(communities, many=True)
-    context = {'data': serializer.data}
+    context['data'] = serializer.data
     return render(request, 'search_community.html', context)
 
 def FilterView(request):
+    context = base_ctx()
     profiles = Profile.objects.all()
     profile_filter = ProfileFilter(request.GET, queryset=profiles)
     profiles = profile_filter.qs
-
-    context = {'profiles' : profiles, 'ProfileFilter' : profile_filter}
+    context['profiles'] = profiles
+    context['ProfileFilter'] = profile_filter
     return render(request, 'filter.html', context)

4544f7655ef2b7821b436d61950174ac9af84470
Maria Stetsenko <maria@stetsenko.su>
2022-05-20 22:30:16 +0300

    added applications in friends, added members in community

--- friends/urls.py
+++ friends/urls.py
@@ -8,8 +8,9 @@ from .views import *
 urlpatterns = [
     path('my_friends/', my_friends, name="my_friends"),
     path('', friends, name="friends"),
+    path('applications/', applications, name="applications"),
     path('is_friend/<int:id>', IsFriend.as_view()),
     path('add_friend/<int:id>', AddFriend.as_view()),
     path('accept_friend/<int:id>', AcceptFriend.as_view()),
-    path('delete_friend/<int:id>', DeleteFriend.as_view())
+    path('delete_friend/<int:id>', DeleteFriend.as_view()),
 ]
